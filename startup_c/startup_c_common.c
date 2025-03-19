5c2b8477374e1c151cf34bca2865b6be8c417bcc
VarvaR <varvar666@gmail.com>
2025-03-19 11:33:20 +0300

    up startup c common

--- startup_c/startup_c_common.c
+++ startup_c/startup_c_common.c
@@ -1,10 +1,10 @@
 #include <stdint.h>
 
-extern uint32_t _sdata;
-extern uint32_t _edata;
-extern uint32_t _sidata;
-extern uint32_t _sbss;
-extern uint32_t _ebss;
+extern uint32_t __data_start;
+extern uint32_t __data_end;
+extern uint32_t __data_flash_start;
+extern uint32_t __bss_start;
+extern uint32_t __bss_end;
 
 /* declaration main function */
 int main(void);
@@ -14,9 +14,9 @@ void __libc_init_array(void);
 void Reset_Handler(void) {
 	// copy .data section from flash to ram
 	/* Copy the data segment initializers from flash to SRAM */
-	uint32_t size = (uint32_t)&_edata - (uint32_t)&_sdata;
-	uint8_t *pRAM = (uint8_t*)&_sdata;
-	uint8_t *pFlash = (uint8_t*)&_sidata;
+	uint32_t size = (uint32_t)&__data_end - (uint32_t)&__data_start;
+	uint8_t *pRAM = (uint8_t*)&__data_start;
+	uint8_t *pFlash = (uint8_t*)&__data_flash_start;
 
 	for(uint32_t i=0; i<size; i++) {
 		pRAM[i] = pFlash[i];
@@ -24,8 +24,8 @@ void Reset_Handler(void) {
 
 	// initialize .bss section
 	/* Zero fill the bss segment. */
-	size = (uint32_t)&_ebss - (uint32_t)&_sbss;
-	pRAM = (uint8_t*)&_sbss;
+	size = (uint32_t)&__bss_end - (uint32_t)&__bss_start;
+	pRAM = (uint8_t*)&__bss_start;
 
 	for(uint32_t i=0; i<size; i++) {
 		pRAM[i] = 0;
@@ -37,6 +37,3 @@ void Reset_Handler(void) {
 	main();
 }
 
-void Default_Handler(void) {
-	while(1) {}
-}
