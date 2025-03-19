c579eec9d855a68b3fc2b597405ac609321917ec
VarvaR <varvar666@gmail.com>
2025-03-19 10:09:06 +0300

    to lowercase

--- to_lovwercase.py
+++ to_lovwercase.py
@@ -1,7 +1,7 @@
 import os
 
-def rename_files_to_lowercase(directory):
-    # Рекурсивно обходим все файлы в директории
+def rename_to_lowercase(directory):
+    # Рекурсивно обходим все файлы и подпапки в директории
     for root, dirs, files in os.walk(directory, topdown=False):
         # Переименовываем файлы
         for file in files:
@@ -15,7 +15,21 @@ def rename_files_to_lowercase(directory):
             # Переименовываем файл, если имя изменилось
             if new_file_name != file:
                 os.rename(current_file_path, new_file_path)
-                print(f"Переименован: {current_file_path} -> {new_file_path}")
+                print(f"Переименован файл: {current_file_path} -> {new_file_path}")
+        
+        # Переименовываем подпапки
+        for dir in dirs:
+            # Получаем текущий путь к подпапке
+            current_dir_path = os.path.join(root, dir)
+            
+            # Преобразуем имя подпапки в lowercase
+            new_dir_name = dir.lower()
+            new_dir_path = os.path.join(root, new_dir_name)
+            
+            # Переименовываем подпапку, если имя изменилось
+            if new_dir_name != dir:
+                os.rename(current_dir_path, new_dir_path)
+                print(f"Переименована папка: {current_dir_path} -> {new_dir_path}")
 
 if __name__ == "__main__":
     # Указываем директорию для обхода
@@ -24,7 +38,7 @@ if __name__ == "__main__":
     # Проверяем, существует ли директория
     if os.path.exists(directory):
         # Запускаем процесс переименования
-        rename_files_to_lowercase(directory)
+        rename_to_lowercase(directory)
         print("Переименование завершено.")
     else:
         print("Указанная директория не существует.")
